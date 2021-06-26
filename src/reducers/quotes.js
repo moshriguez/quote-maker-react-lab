00c26b562476a73044d2d5266d7b7942ad064f8b
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-06-25 18:15:23 -0700

    commit tree 7d0fa84e0d8d32cd95ef0fcbf1fdae688d817422

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,39 @@
-export default (state = [], action) => {
-  return state;
+export default function quotesReducer(state = [], action) {
+  let index
+  let newState
+
+  switch (action.type) {
+    case "ADD_QUOTE":
+      return [...state, action.quote]
+  
+    case "REMOVE_QUOTE":
+      index = state.findIndex(quote => quote.id === action.quoteId)
+      return [...state.slice(0, index), ...state.slice(index + 1)]
+    
+    case "UPVOTE_QUOTE":
+      newState = [...state]
+      return newState.map(quote => {
+        if (quote.id === action.quoteId) {
+          quote.votes += 1
+          return quote
+        } else {
+          return quote
+        }
+      })
+
+    case "DOWNVOTE_QUOTE":
+      newState = [...state]
+      return newState.map(quote => {
+        if (quote.id === action.quoteId && quote.votes > 0) {
+          quote.votes -= 1
+          return quote
+        } else {
+          return quote
+        }
+      })
+
+
+    default:
+      return state;
+  }
 }
